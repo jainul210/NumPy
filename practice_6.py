22878edde3968c7c4948a529922d4acc02bc3c63
JAINUL ABDEEN <jainulabdeen210@gmail.com>
2026-07-09 23:05:57 +0530

    Remove commented-out print statement in practice_6.py for cleaner code

--- practice_6.py
+++ practice_6.py
@@ -1,9 +1,7 @@
 # Create a 3-D array and perform operations on it.
 import numpy as np
 # arr = np.arange(6).reshape(2,3)
-# print(arr)
 arr = np.arange(27).reshape(3,3,3)
-
 element = arr[1,1,2]
 sum = arr.sum(axis=0)
 print(sum)
